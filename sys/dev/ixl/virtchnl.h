fb10aef2e51e77fe10e729fec8770ba25dcf34b8
Alan Brady <alan.brady@intel.com>
2017-04-27 15:52:44 -0700

    virtchnl: add VIRTCHNL_OP_REQUEST_QUEUES
    This patch adds a new virtchnl OP which will allow the VF to negotiate
    with the PF how many queues the PF has allocated for that VF.  In the
    current scheme each VF gets a default number of queues and the VF can
    choose to enable any/all of those allocated queues.  This message is
    intended to be used to request a differential number allocated queues
    for that particular VF.  Without this new message there is no way for
    the VF to request more queues than what it is allocated by default.
    
    Signed-off-by: Alan Brady <alan.brady@intel.com>

--- sys/dev/ixl/virtchnl.h
+++ sys/dev/ixl/virtchnl.h
@@ -145,6 +145,7 @@ enum virtchnl_ops {
 	VIRTCHNL_OP_SET_RSS_HENA = 26,
 	VIRTCHNL_OP_ENABLE_VLAN_STRIPPING = 27,
 	VIRTCHNL_OP_DISABLE_VLAN_STRIPPING = 28,
+	VIRTCHNL_OP_REQUEST_QUEUES = 29,
 
 };
 
@@ -309,6 +310,21 @@ struct virtchnl_vsi_queue_config_info {
 	u16 vsi_id;
 	u16 num_queue_pairs;
 	struct virtchnl_queue_pair_info qpair[1];
+};
+
+/* VIRTCHNL_OP_REQUEST_QUEUES
+ * VF sends this message to request the PF to allocate additional queues to
+ * this VF.  Each VF gets a guaranteed number of queues on init but asking for
+ * additional queues must be negotiated.  This is a best effort request as it
+ * is possible the PF does not have enough queues left to support the request.
+ * If the PF cannot support the number requested it will respond with the
+ * maximum number it is able to support; otherwise it will respond with the
+ * number requested.
+ */
+
+/* VF resource request */
+struct virtchnl_vf_res_request {
+	u16 num_queue_pairs;
 };
 
 /* VIRTCHNL_OP_CONFIG_IRQ_MAP
@@ -657,6 +673,9 @@ virtchnl_vc_validate_vf_msg(struct virtchnl_version_info *ver, u32 v_opcode,
 		break;
 	case VIRTCHNL_OP_ENABLE_VLAN_STRIPPING:
 	case VIRTCHNL_OP_DISABLE_VLAN_STRIPPING:
+		break;
+	case VIRTCHNL_OP_REQUEST_QUEUES:
+		valid_len = sizeof(struct virtchnl_vf_res_request);
 		break;
 	/* These are always errors coming from the VF. */
 	case VIRTCHNL_OP_EVENT:
