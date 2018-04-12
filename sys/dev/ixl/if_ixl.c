e10bd0431185c062ebe88b73987024e36c1ca44d
Eric Joyner <erj@freebsd.org>
2018-04-12 09:32:55 -0700

    ixl(4): Replace some numbers with constants
    Signed-off-by: Eric Joyner <erj@freebsd.org>

--- sys/dev/ixl/if_ixl.c
+++ sys/dev/ixl/if_ixl.c
@@ -315,13 +315,13 @@ extern struct if_txrx ixl_txrx_dwb;
 
 static struct if_shared_ctx ixl_sctx_init = {
 	.isc_magic = IFLIB_MAGIC,
-	.isc_q_align = PAGE_SIZE,/* max(DBA_ALIGN, PAGE_SIZE) */
+	.isc_q_align = PAGE_SIZE,
 	.isc_tx_maxsize = IXL_TSO_SIZE,
-	.isc_tx_maxsegsize = 16383,
+	.isc_tx_maxsegsize = IXL_MAX_DMA_SEG_SIZE,
 
 	.isc_rx_maxsize = 16384,
-	.isc_rx_nsegments = 5,
-	.isc_rx_maxsegsize = 16383,
+	.isc_rx_nsegments = IXL_MAX_RX_SEGS,
+	.isc_rx_maxsegsize = IXL_MAX_DMA_SEG_SIZE,
 	.isc_nfl = 1,
 	.isc_ntxqs = 1,
 	.isc_nrxqs = 1,
