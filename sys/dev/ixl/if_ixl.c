e2cad3ec9080bf4eca0eef740ac49152907edd86
Eric Joyner <erj@freebsd.org>
2018-08-23 14:15:17 -0700

    ixl(4): Fix memory leak when head writeback is disabled
    In ixl_if_queues_free(), the TX RSQ memory is supposed to be freed
    when head writeback is disabled (because it's only used in
    descriptor writeback mode), instead of the other way around.
    
    Signed-off-by: Eric Joyner <erj@freebsd.org>

--- sys/dev/ixl/if_ixl.c
+++ sys/dev/ixl/if_ixl.c
@@ -1136,7 +1136,7 @@ ixl_if_queues_free(if_ctx_t ctx)
 	struct ixl_pf *pf = iflib_get_softc(ctx);
 	struct ixl_vsi *vsi = &pf->vsi;
 
-	if (vsi->enable_head_writeback) {
+	if (!vsi->enable_head_writeback) {
 		struct ixl_tx_queue *que;
 		int i = 0;
 
